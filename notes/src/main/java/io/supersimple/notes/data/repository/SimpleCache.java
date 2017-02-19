d189802b9801b7fcfc9648bbebcac7b78df67cbe
Arjan Duijzer <arjan@nxtstep.nl>
2017-02-19 15:22:21 +0100

    SimpleCache

---
+++ notes/src/main/java/io/supersimple/notes/data/repository/SimpleCache.java
@@ -0,0 +1,75 @@
+package io.supersimple.notes.data.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+public abstract class SimpleCache<K, V> {
+    private final Map<K, V> cache;
+
+    public SimpleCache() {
+        cache = new HashMap<>();
+    }
+
+    public SimpleCache(Map<K, V> cache) {
+        this.cache = cache;
+    }
+
+    public Maybe<V> get(K id) {
+        return Maybe.create(observer -> {
+            if (cache.containsKey(id)) {
+                observer.onSuccess(cache.get(id));
+            }
+            observer.onComplete();
+        });
+    }
+
+    public Maybe<List<V>> list() {
+        return Maybe.create(observer -> {
+            if (!cache.isEmpty()) {
+                List<V> list = new ArrayList<>(cache.values());
+                observer.onSuccess(list);
+            }
+            observer.onComplete();
+        });
+    }
+
+    public Single<V> save(V value) {
+        return Single.create(observer -> {
+            cache.put(getId(value), value);
+            observer.onSuccess(value);
+        });
+    }
+
+    public Maybe<V> delete(K id) {
+        return Maybe.create(observer -> {
+            if (cache.containsKey(id)) {
+                observer.onSuccess(cache.remove(id));
+            }
+            observer.onComplete();
+        });
+    }
+
+    public Maybe<V> deleteValue(V value) {
+        return delete(getId(value));
+    }
+
+    public Single<List<V>> deleteAll() {
+        return list()
+                .flatMapObservable(Observable::fromIterable)
+                .map(this::getId)
+                .flatMapMaybe(this::delete)
+                .toList();
+    }
+
+    public void clear() {
+        cache.clear();
+    }
+
+    abstract K getId(V value);
+}
---
+++ notes/src/test/java/io/supersimple/notes/data/repository/SimpleCacheTest.java
@@ -0,0 +1,78 @@
+package io.supersimple.notes.data.repository;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import io.supersimple.notes.data.models.Note;
+
+public class SimpleCacheTest {
+
+    static class TestCache extends SimpleCache<String, Note> {
+
+        @Override
+        String getId(Note value) {
+            return value.id();
+        }
+    }
+
+    @Test
+    public void testSimpleMemCache() {
+        TestCache cache = new TestCache();
+
+        //
+        // Save
+        cache.save(Note.create("1", "Test me"))
+                .test()
+                .assertResult(Note.create("1", "Test me"));
+
+        //
+        // Get
+        cache.get("1")
+                .test()
+                .assertResult(Note.create("1", "Test me"));
+
+        cache.get("2")
+                .test()
+                .assertNoValues()
+                .assertNoErrors()
+                .assertComplete();
+
+        cache.list()
+                .test()
+                .assertResult(Arrays.asList(Note.create("1", "Test me")));
+        //
+        // Delete
+        cache.delete("2")
+                .test()
+                .assertNoValues()
+                .assertNoErrors()
+                .assertComplete();
+
+        cache.delete("1")
+                .test()
+                .assertResult(Note.create("1", "Test me"));
+
+        cache.deleteValue(Note.create("1", "Test me"))
+                .test()
+                .assertNoValues()
+                .assertNoErrors()
+                .assertComplete();
+
+        //
+        // Clear
+        cache.save(Note.create("2", "Whatever value"))
+                .test()
+                .assertValueCount(1)
+                .assertNoErrors()
+                .assertComplete();
+
+        cache.clear();
+
+        cache.list()
+                .test()
+                .assertNoValues()
+                .assertNoErrors()
+                .assertComplete();
+    }
+}
