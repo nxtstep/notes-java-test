9faf1233f3a0636918291b125f077d84880d69d2
Arjan Duijzer <arjan@nxtstep.nl>
2017-02-19 15:22:52 +0100

    NoteListActivity as Launcher/default activity

---
+++ notes/src/main/java/io/supersimple/notes/presentation/list/NoteListActivity.java
@@ -0,0 +1,15 @@
+package io.supersimple.notes.presentation.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.supersimple.notes.R;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+    }
+}
